252f0e91129cbcaef8fb6b51dbe934641909401b
irainw <rainwater@slac.stanford.edu>
2026-06-24 11:26:36 -0700

    added the DataPreprocessor Class and moved the Preprocessing Func into it.

--- preprocessing_data.py
+++ preprocessing_data.py
@@ -2,44 +2,54 @@ import numpy as np
 import pandas as pd
 from tensorflow import keras
 
-def preprocessing(df, features, label):
-    """ Data preprocesing
-    Produce normalizer, test and train dataframes ready for input to keras
+class DataPreprocessor:
+    def __init__(self, df: dataFrame, features: list, label: list):
+        self.df = df
+        self.features = features
+        self.label = label
 
-    Args:
+    def preprocessing(self.df, self.features, self.label):
+        """ Data preprocesing
+        Produce normalizer, test and train dataframes ready for input to keras
+
+        Args:
         df (dataframe): pandas dataframe. Input data
         features (list): list of strings. List of features
         label (scalar): string. Label used as target
 
-    Returns:
+        Returns:
         tuple: normalizer, test and train dataframes 
-    """
+        """
     
-    df = df[df["errcode"] == 0]
-    print(f"Number of lines after quality cuts: {len(df)}")
+        df = self.df
+        features = self.features
+        label = self.label
 
-    df = df.rename(columns={"x_core'": "x_core"})
+        df = df[df["errcode"] == 0]
+        print(f"Number of lines after quality cuts: {len(df)}")
 
-    # Radius
-    df["R_p"] = df["R_a"] + df["R_b"]
+        df = df.rename(columns={"x_core'": "x_core"})
 
-    # Mass
-    df["M_p"] = df["M_a"] + df["M_b"]
+        # Radius
+        df["R_p"] = df["R_a"] + df["R_b"]
 
-    # train and test sample (80/20 split)
-    train_dataset = df.sample(frac=0.8, random_state=0)
-    test_dataset = df.drop(train_dataset.index)
+        # Mass
+        df["M_p"] = df["M_a"] + df["M_b"]
 
-    # features = what it knows/what it will predict on
-    train_features = train_dataset[features].copy()
-    test_features = test_dataset[features].copy()
+        # train and test sample (80/20 split)
+        train_dataset = df.sample(frac=0.8, random_state=0)
+        test_dataset = df.drop(train_dataset.index)
 
-    # labels = what it is predicting
-    train_labels = train_dataset[label].copy()
-    test_labels = test_dataset[label].copy()
+        # features = what it knows/what it will predict on
+        train_features = train_dataset[features].copy()
+        test_features = test_dataset[features].copy()
 
-    # The layer is now ready to normalize future data 
-    normalizer = keras.layers.Normalization(axis=-1)
-    normalizer.adapt(np.array(train_features))
+        # labels = what it is predicting
+        train_labels = train_dataset[label].copy()
+        test_labels = test_dataset[label].copy()
 
-    return normalizer, train_features, test_features, train_labels, test_labels
+        # The layer is now ready to normalize future data 
+        normalizer = keras.layers.Normalization(axis=-1)
+        normalizer.adapt(np.array(train_features))
+
+        return normalizer, train_features, test_features, train_labels, test_labels
